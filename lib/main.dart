a438fb222ce5350e9cd9b44913c55bb6c6adf1f3
virgiawankusuma <virgiawan150@gmail.com>
2021-06-26 00:19:33 +0700

    general page

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'ui/pages/pages.dart';
+// import 'ui/pages/pages.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GeneralPage(onBackButtonPressed: () {}, child: Text("body")),
+    );
+  }
+}
---
+++ lib/ui/pages/pages.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pertemuan_9/shared/shared.dart';
+import 'package:supercharged/supercharged.dart';
+
+part 'general_page.dart';
