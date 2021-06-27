a571ab641b03fac33d02e16b75a43eb1399eecca
virgiawankusuma <virgiawan150@gmail.com>
2021-06-27 21:38:29 +0700

    custom tab bar

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:food_market/models/models.dart';
 import 'package:food_market/shared/shared.dart';
+import 'package:get/get.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:supercharged/supercharged.dart';
 
 part 'custom_bottom_navbar.dart';
+part 'custom_tabbar.dart';
 part 'food_card.dart';
 part 'rating_stars.dart';
