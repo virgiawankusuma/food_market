b8108f721d1e36f3a5f6bfecf630c3bd06686c31
virgiawankusuma <virgiawan150@gmail.com>
2021-06-28 13:32:35 +0700

    food list item

--- lib/ui/widgets/widgets.dart
+++ lib/ui/widgets/widgets.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:food_market/models/models.dart';
 import 'package:food_market/shared/shared.dart';
-import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+// import 'package:get/get.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:supercharged/supercharged.dart';
 
@@ -9,3 +10,4 @@ part 'custom_bottom_navbar.dart';
 part 'custom_tabbar.dart';
 part 'food_card.dart';
 part 'rating_stars.dart';
+part 'food_list_item.dart';
