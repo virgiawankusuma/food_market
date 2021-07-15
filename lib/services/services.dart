1f2fb8f58145c89cbab1ce5b42457195204da007
virgiawankusuma <virgiawan150@gmail.com>
2021-07-16 03:59:15 +0700

    food market services

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,4 +1,6 @@
+import 'package:food_market/models/models.dart';
 
 part 'user_services.dart';
 part 'food_services.dart';
+
 part 'transaction_services.dart';
--- lib/services/transaction_services.dart
+++ lib/services/transaction_services.dart
@@ -7,9 +7,12 @@ class TransactionServices {
     return ApiReturnValue(value: mockTransaction);
   }
 
-  static Future<ApiReturnValue<Transiction>> submitTransiction(Transiction transiction) async {
+  static Future<ApiReturnValue<Transaction>> submitTransiction(
+      Transaction transaction) async {
     await Future.delayed(Duration(seconds: 2));
 
-    return ApiReturnValue(value: transaction.copyWith(id:123, status: Transaction.pending));
+    return ApiReturnValue(
+        value:
+            transaction.copyWith(id: 123, status: TransactionStatus.pending));
   }
 }
